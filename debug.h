3617ca7c27d68d6f9d8c4c1f1b6b0f327dd5446c
Markus Fröschle <mfro@mubf.de>
2021-03-14 12:03:43 +0100

    add dbg facility
    uses natfeats when m68k, console output when ColdFire

--- debug.h
+++ debug.h
@@ -2,9 +2,15 @@
 #define DEBUG_H
 
 #ifdef DEBUG
+#ifdef __mcoldfire__
 #include <stdio.h>
 #define dbg(format, arg ...) do { printf("DEBUG: (%s):" format, __FUNCTION__, ##arg); } while (0)
 #define out(format, arg ...) do { printf("" format, ##arg); } while (0)
+#else
+#include "natfeats.h"
+#define dbg(format, arg ...) do { nf_printf("DEBUG: (%s):" format, __FUNCTION__, ##arg); } while (0)
+#define out(format, arg ...) do { nf_printf("DEBUG: (%s):" format, __FUNCTION__, ##arg); } while (0)
+#endif
 #else
 #define dbg(format, arg ...) /* */
 #define out(format, arg ...) do { printf("" format, ##arg); } while (0)
